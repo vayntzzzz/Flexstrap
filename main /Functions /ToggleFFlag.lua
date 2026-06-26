3a93894a4cb237bc18aff80cc0dc3de46e80afef
vayntzzzz <kilaniraed5@gmail.com>
2026-06-26 15:47:17 +0200

    Create ToggleFFlag.lua

---
+++ main /Functions /ToggleFFlag.lua
@@ -0,0 +1,17 @@
+local cloneref = cloneref or function(...) return ... end
+local HttpService = cloneref(game.GetService(game, "HttpService"))
+return function(flag: string, value: string): (string, string) -> ()
+	local type = type or typeof
+	if type(flag) ~= "string" then return task.spawn(error, "string expected, got "..type(flag)) end
+	local FFlag: string = Flexstrap.TouchEnabled and flag:gsub("DFInt", ""):gsub("DFFlag", ""):gsub("FFlag", ""):gsub("FInt", ""):gsub("DFString", ""):gsub("FString", "") or flag --> Removes the keyword of the FFlag, setfflag doesn't like those so we will need to remove it.
+	
+	if getfflag(FFlag) ~= nil then
+		local fflagfile = HttpService:JSONDecode(readfile("Flexstrap/FFlags.json"))
+		fflagfile[flag] = tostring(value)
+		writefile("Flexstrap/FFlags.json", HttpService:JSONEncode(fflagfile))
+		return setfflag(FFlag, tostring(value))
+	else
+		local err = isfile(errorlog) and readfile(errorlog) or "Error while loading FFlags: "
+		return writefile(errorlog, err.."\nFFlag expected, got "..FFlag)
+	end
+end;
